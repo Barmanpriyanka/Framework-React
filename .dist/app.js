a616df2e707a62f95c07935f08c4b590f2449f2e
Barmanpriyanka <priyankabarman1604@gmail.com>
2024-06-03 22:50:51 +0530

    React day2

---
+++ .dist/app.js
@@ -0,0 +1,40 @@
+
+
+/*<div>
+<div id="child1">
+ <h1></h1>
+</div>
+
+
+<div  id="chid2">
+ <h1></h1>
+</div>
+
+</div>*/
+const parent=React.createElement(
+    "div",
+    {id:"parent"}, 
+    [
+        React.createElement(
+            "div",
+            {id:"child1"},[
+            React.createElement("h1",{},"This is h1 tag inside child1"),
+            React.createElement("h2",{},"This is h2 tag inside child1")]) ,
+        
+            React.createElement(
+                "div",
+                {id:"child2"},[
+                React.createElement("h1",{},"This is h1 tag inside child2"),
+                React.createElement("h2",{},"This is h2 tag inside child2")]) ,
+                
+
+     ]        
+
+    )
+
+   
+
+       console.log(parent);
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
+    
